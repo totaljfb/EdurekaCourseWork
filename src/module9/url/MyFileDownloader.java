fec9559a53f9f007ccf7c4a1a3a664892dc1e872
totaljfb <totaljfb@gmail.com>
2018-10-18 23:21:09 -0500

    module9 update6

--- src/module9/url/MyFileDownloader.java
+++ src/module9/url/MyFileDownloader.java
@@ -6,11 +6,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.text.DecimalFormat;
 
 public class MyFileDownloader {
 
 	private static String spec = "http://www.oracle.com/events/global/en"
 			+ "/java-outreach/resources/java-a-beginners-guide-1720064.pdf";
+	private static float percent;
+	private static int number_of_backspace;
 	
 	public static void main(String[] args) {
 		if(args.length == 1) {
@@ -21,13 +25,16 @@ public class MyFileDownloader {
 					URL url = new URL(spec);
 					int count = -1;
 					byte[] buffer = new byte[20480];
+					URLConnection conn = url.openConnection();
+					int filesize = conn.getContentLength();
 					InputStream istream = url.openStream();
 					//get full file name from url
 					String filename = spec.substring(spec.lastIndexOf('/')+1, spec.length() );
-					//construct the file with given path
-					FileOutputStream fos = new FileOutputStream(file.getPath() + "\\"+ filename);
+					//construct the file with given path, always overwrite
+					FileOutputStream fos = new FileOutputStream(file.getPath() + "\\"+ filename, false);
 					while((count = istream.read(buffer)) != -1) {
 						fos.write(buffer,0,count);
+						print_progress(count,filesize);
 					}
 					fos.flush();
 					fos.close();
@@ -48,6 +55,16 @@ public class MyFileDownloader {
 			System.out.println("[path] is the folder path where the pdf file is downloaded");
 			System.exit(-1);
 		}	
+	}
+	
+	public static void print_progress(int count, int filesize) {
+		for(int i = 0; i< number_of_backspace; i++) {
+			System.out.print("\b");
+		}
+		percent = ((float)count)/((float)filesize)*100f + percent;
+		DecimalFormat df = new DecimalFormat("###.##");
+		System.out.print(df.format(percent) + "%");
+		number_of_backspace = df.format(percent).length();
 	}
 
 }
