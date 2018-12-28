68fa5f78fae8e7aa2e098ec28421471c2bebd70e
totaljfb <totaljfb@gmail.com>
2018-12-28 07:59:03 -0600

    final project update3

--- src/advanced/certificate/project/EmployeeAttendanceTracking.java
+++ src/advanced/certificate/project/EmployeeAttendanceTracking.java
@@ -14,12 +14,13 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 import org.apache.log4j.Logger;
-
+import org.apache.logging.log4j.core.parser.ParseException;
 
 import module9.tcp.sockets.Client;
 
@@ -34,7 +35,7 @@ public class EmployeeAttendanceTracking {
 	private static final SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
 	private static final SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM");
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws java.text.ParseException {
 		
 		Properties p = new Properties();
 		//load the property file
@@ -75,7 +76,16 @@ public class EmployeeAttendanceTracking {
 						System.out.println("For example, 2018-05-07 or 2018-05");
 						origin_input = sc.next();
 						logger.info("User input: " + origin_input);
-						try
+						if(origin_input.split("-").length == 2) {
+							Date date = sdf2.parse(origin_input.trim());					
+							go_query(conn, origin_input.trim(), "1month");
+						}else if(origin_input.split("-").length == 3) {
+							Date date = sdf1.parse(origin_input.trim());
+							go_query(conn, origin_input.trim(), "1day");
+						}else {
+							logger.info("Wrong input, program ended");
+							System.exit(-1);
+						}
 						break;
 				case 2: System.out.println("Please specify a day or a month and an an employee's initials to query");
 						System.out.println("for example: 2018-11-28, I N");
@@ -89,7 +99,7 @@ public class EmployeeAttendanceTracking {
 							input_list.clear();
 							input_list.add(s[0].trim());
 							input_list.add(s[1].trim());
-							go_query(2, conn, input_list, origin_input);
+							//go_query(2, conn, input_list, origin_input);
 						}
 						else {
 							logger.info("Wrong input format, please try again");
@@ -124,7 +134,7 @@ public class EmployeeAttendanceTracking {
 							}catch(Exception e){
 								e.printStackTrace();
 							}
-							go_query(3, conn, input_list, origin_input);
+							//go_query(3, conn, input_list, origin_input);
 						}
 						else {
 							logger.info("Wrong input format, please try again");
@@ -148,21 +158,19 @@ public class EmployeeAttendanceTracking {
 	}
 	
 	//prepare the sql query	
-	public static void go_query(int case_number, Connection conn, List<String> input_list, String origin_input) {
-		String parse_result = null;
-		parse_result = parse_date(input_list.get(0));
+	public static void go_query(Connection conn, String input_date, String case_op) {
 		String basic_sql = "select emp_name, time, code FROM test.attendance_log AS a, test.emp_access_map AS b, test.emp AS c  "
 							+ "where a.card_id = b.card_id "
 							+ "and b.emp_id = c.emp_id ";
 		PreparedStatement pstmt = null;
 		String sql = null;
-		switch(case_number + parse_result) {
+		switch(case_op) {
 		//use the join table to display employee initials rather than employee id
 		case "1day":sql = basic_sql + "and time between ? and ? ";
 					try {
 						pstmt = conn.prepareStatement(sql);
-						pstmt.setString(1, input_list.get(0));
-						pstmt.setString(2, input_list.get(0) + " 23:59:59");
+						pstmt.setString(1, input_date);
+						pstmt.setString(2, input_date + " 23:59:59");
 						execute_sql(pstmt); 
 					} catch (SQLException e) {
 						e.printStackTrace();
@@ -171,7 +179,7 @@ public class EmployeeAttendanceTracking {
 		case "1month":sql = basic_sql + "and time like ? ";
 					  try {
 						  pstmt = conn.prepareStatement(sql);
-						  pstmt.setString(1, input_list.get(0) + "%");
+						  pstmt.setString(1, input_date + "%");
 						  execute_sql(pstmt); 
 					  } catch (SQLException e) {
 						  e.printStackTrace();
@@ -200,38 +208,7 @@ public class EmployeeAttendanceTracking {
 		break;
 		default: System.exit(-1);
 		}
-	}
-	
-	//validate the input date format
-	public static String parse_date(String date) {
-		long count_ =  date.chars().filter(ch -> ch == '-').count();
-		String parse_result = null;
-		if(count_ == 1L) {
-			try {
-				formatter1.parse(date);
-				parse_result = "month";
-			}catch(DateTimeParseException e) {
-				logger.info("Wrong format or invalid date, please check your date and use yyyy-mm-dd or yyyy-mm format");
-				logger.info("Program ended");
-				System.exit(-1);
-			}
-		}
-		else if(count_ == 2L) {
-			try {
-				formatter2.parse(date);
-				parse_result = "day";
-			}catch(DateTimeParseException e) {
-				logger.info("Wrong format or invalid date, please check your date and use yyyy-mm-dd or yyyy-mm format");
-				logger.info("Program ended");
-				System.exit(-1);
-			}
-		}
-		else {
-			logger.info("Wrong format or invalid date, please check your date and use yyyy-mm-dd or yyyy-mm format");
-			logger.info("Program ended");
-			}
-		return parse_result;
-	}
+	}	
 	
 	//execute query
 	public static void execute_sql(PreparedStatement pstmt) throws SQLException {
